a3194431c6c9ac6d34fade833e4bb096d3f1a7a4
Danick Triantis <danick@triantis.nl>
2023-11-30 14:17:33 +0100

    Database is now opened and closed on demand
    Database is never kept open, so that it is only open when database access is actually needed.

--- Application/leaderboard.py
+++ Application/leaderboard.py
@@ -4,56 +4,78 @@ import os
 
 class Leaderboard:
     def __init__(self, path):
+        self.path = path
+
         if not os.path.exists(path):
             f = open(path, "x")
             print("Database created at: " + path)
     
         print("Connecting to Database: " + path)
-        self.db = sqlite3.connect(path)
+        db = self.open_database()
 
-        cursor = self.db.cursor()
+        cursor = db.cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS leaderboard(name, score)")
+        self.close_database(db)
+        print("Database initialisation complete.")
 
+    def open_database(self):
+        db = sqlite3.connect(self.path)
+        return db
+    
+    def close_database(self, db):
+        db.commit()
+        db.close()
+        
 
     def update_leaderboard(self, data):
         """
         Update the leaderboard with new data.
         :param data: List of tuples (name, reps)
         """
+        db = self.open_database()
+
         for entry in data:        
-            self.insert_new_entry(entry[0], entry[1])
+            self.insert_new_entry(db, entry[0], entry[1])
+
+        self.close_database(db)
 
     def get_leaderboard_data(self):
         """
         Get the current leaderboard data.
         :return: List of tuples (name, reps) ordered by reps in descending order
         """
-        cursor = self.db.cursor()
+        db = self.open_database()
+        cursor = db.cursor()
+
         result = cursor.execute("SELECT * FROM leaderboard")
+        output = result.fetchall()
 
-        return result.fetchall()
+        self.close_database(db)
 
-    def insert_new_entry(self, name, reps):
+        return output
+
+    def insert_new_entry(self, db, name, reps):
         """
         Insert a new entry into the leaderboard.
         :param name: Name of the entry
         :param reps: Number of reps for the entry
         """
-        cursor = self.db.cursor()
+        cursor = db.cursor()
         cursor.execute("INSERT INTO leaderboard VALUES" + "('" + name + "'," + str(reps) + ")")
-        self.db.commit()
+        db.commit()
 
     def get_min_score(self):
         """
         Get the minimum score in the leaderboard.
         :return: Minimum score
         """
-        cursor = self.db.cursor()
-        result = cursor.execute("SELECT * FROM leaderboard ORDER BY score ASC LIMIT 1")
+        db = self.open_database()
+        cursor = db.cursor()
 
-        return result.fetchone()[1]
+        result = cursor.execute("SELECT * FROM leaderboard ORDER BY score ASC LIMIT 1")
+        output = result.fetchone()[1]
+
+        self.close_database(db)
+
+        return output
     
-    def close(self):
-        self.db.commit()
-        self.db.close()
-        print("Database Connection Closed")
