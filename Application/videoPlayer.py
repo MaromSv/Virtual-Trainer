b638f0556326a5affce4f2d0865036e8683f1ed0
MaromSv <118516370+MaromSv@users.noreply.github.com>
2023-12-04 11:04:21 +0100

    new video, resizing works now

--- Application/videoPlayer.py
+++ Application/videoPlayer.py
@@ -5,8 +5,8 @@ import pygame
 
 
 def playVideo(path):
-    clip = VideoFileClip(path)
-    clip.preview().resize(1.5)
+    clip = VideoFileClip(path).resize(1.5)
+    clip.preview()
     pygame.quit()
 
-playVideo('Application\\Assets\\Video\\baby.mp4')
+# playVideo('Application\\Assets\\Video\\baby.mp4')
