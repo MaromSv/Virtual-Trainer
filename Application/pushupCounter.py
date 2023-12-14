16a4f7efafb7d9e2fb865eef4051614b8f6be73c
MaromSv <118516370+MaromSv@users.noreply.github.com>
2023-12-14 14:51:17 +0100

    back straight fixed

--- Application/pushupCounter.py
+++ Application/pushupCounter.py
@@ -40,7 +40,7 @@ def straight_Back(leftShoulder, rightShoulder, rightHip, leftHip):
 
     distLeft = distance_two_points(leftShoulder, leftHip)
 
-    if (distLeft + distRight < 0.5):
+    if (distLeft + distRight < 0.3):
         return True
     else:
         return False
