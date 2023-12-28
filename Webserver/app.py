dfaf1fd4ecda3996e208a1b3b5a7fedcf72e3188
MariosPapa13 <m.papalouka@student.tue.nl>
2023-12-28 17:34:46 +0100

    aa

--- Webserver/app.py
+++ Webserver/app.py
@@ -1,33 +1,63 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+# import sqlite_web
+import requests
+import json
+
+# test_url = "http://danick.triantis.nl:8080/users/query/?sql=INSERT+INTO+users+(chk_email,+chk_password,+chk_userid)+VALUES+(%27marios%27,+%27m1234%27,+%271%27);"
+# x = requests.get(test_url)
+
+import requests
+
+# url = 'http://danick.triantis.nl:8080'
+# data = {'query': 'SELECT * FROM leaderboard'}
+
+# response = requests.get(url, json=data)
+
+# print(response.json())
+
+# url = "http://danick.triantis.nl:8080/leaderboard/query/?sql=SELECT+*+FROM+users;"
 
 
+def get_password(email):
+    email.replace("@", "%40")
+    email = "%22" + email + "%22"
+    url = "http://danick.triantis.nl:8080/users/query/?ordering=&export_json=&sql=SELECT+password+FROM+%22users%22+WHERE+email%3D{}".format(email)
+    x = requests.get(url)
+    password_db = json.loads(x.text)[0]["password"]
+    print(password_db)
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///root/Database/database.db'
-db = SQLAlchemy(app)
+def get_leaderboard():
+    url = "http://danick.triantis.nl:8080/leaderboard/query/?ordering=&export_json=&sql=SELECT+*+FROM+%22leaderboard%22;"
+    x = requests.get(url)
+    data = json.loads(x.text)
+    print(data)
 
-class User(db.Model):
-    userid = db.Column(db.Integer, primary_key=True, nullable=True)
-    email = db.Column(db.text(120), unique=True, nullable=True)
-    password = db.Column(db.text(255), nullable=True)
+# get_leaderboard()
+# get_password("marios@gmail.com")
 
-class Personal_Form(db.Model):
-    userid = db.Column(db.Integer, primary_key=True, nullable=True)
-    first_name = db.Column(db.text(255), nullable=True)
-    last_name = db.Column(db.text(255), nullable=True)
-    age = db.Column(db.Integer, nullable=True)
-    gender = db.Column(db.text(255), nullable=True)
-    experience = db.Column(db.text(255), nullable=True)
-    location = db.Column(db.text(255), nullable=True)
+app = Flask(__name__,template_folder='templates',static_folder='static')
 
-class Buddy_Form(db.Model):
-    userid = db.Column(db.Integer, primary_key=True, nullable=True)
-    days_preference = db.Column(db.text(255), nullable=True)
-    gender_preference = db.Column(db.text(255), nullable=True)
-    experience_preference = db.Column(db.text(255), nullable=True)
-    location_preference = db.Column(db.text(255), nullable=True)
+# class User(db.Model):
+#     userid = db.Column(db.Integer, primary_key=True, nullable=True)
+#     email = db.Column(db.text(120), unique=True, nullable=True)
+#     password = db.Column(db.text(255), nullable=True)
+
+# class Personal_Form(db.Model):
+#     userid = db.Column(db.Integer, primary_key=True, nullable=True)
+#     first_name = db.Column(db.text(255), nullable=True)
+#     last_name = db.Column(db.text(255), nullable=True)
+#     age = db.Column(db.Integer, nullable=True)
+#     gender = db.Column(db.text(255), nullable=True)
+#     experience = db.Column(db.text(255), nullable=True)
+#     location = db.Column(db.text(255), nullable=True)
+
+# class Buddy_Form(db.Model):
+#     userid = db.Column(db.Integer, primary_key=True, nullable=True)
+#     days_preference = db.Column(db.text(255), nullable=True)
+#     gender_preference = db.Column(db.text(255), nullable=True)
+#     experience_preference = db.Column(db.text(255), nullable=True)
+#     location_preference = db.Column(db.text(255), nullable=True)
 
 
 
@@ -53,33 +83,18 @@ def buddy():
 
 @app.route('/leaderboard')
 def leaderboard():
+    get_leaderboard()
     return render_template('leaderboard.html')
 
 @app.route('/login_form', methods=["get"])
 def login_form():
     email = request.form['loginemail']
     password = request.form['loginpw']
-    user = User.query.filter_by(email=email).first()
-    if user and password == user.password:
+    # user = User.query.filter_by(email=email).first()
+    if password == get_password(email):
         return render_template('home.html')
     else:
         return render_template('login.html')
-
-
-
-# def get_user_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = sqlite3.connect('/root/Database/database.db')
-#         cur = db.cursor()
-#         cur.execute('SELECT * FROM leaderboard')
-#         db = cur.fetchall()
-#     return db
-
-# def close_connection(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
